01004a9be93d9d8f7ceced7758fba9f23ed3a0c6
Cooney <ahwhw3@splm.siemens.com>
2022-03-18 12:32:11 +0100

    scandit barcode scanner added

--- README.md
+++ README.md
@@ -2,13 +2,17 @@
 [A mendix wrapper for the scandit code scanner ]
 
 ## Features
-[feature highlights]
+- Scan multiple barcodes with a device camera
 
 ## Usage
-[step by step instructions]
+- For development configure the project path in `package.json`
+- For development run `npm run dev` -> changes will be automatically rendered to the mendix application
+- For building the project run `npm run build`
 
-## Demo project
-[link to sandbox]
+## Resources
+- [NPM react sdk](https://www.npmjs.com/package/scandit-sdk-react)
+- [Documentation](https://docs.scandit.com/stable/web/)
+- [Scandit home page](https://www.scandit.com/developers/)
 
 ## Issues, suggestions and feature requests
 [link to GitHub issues]
--- src/Scanditwrapper.jsx
+++ src/Scanditwrapper.jsx
@@ -1,10 +1,47 @@
 import { Component, createElement } from "react";
+import ScanditBarcodeScanner from "scandit-sdk-react";
+import {
+    Barcode,
+    BarcodePicker,
+    Camera,
+    CameraAccess,
+    CameraSettings,
+    ScanSettings,
+    SingleImageModeSettings,
+  } from "scandit-sdk";
 
 import { HelloWorldSample } from "./components/HelloWorldSample";
 import "./ui/Scanditwrapper.css";
 
 export default class Scanditwrapper extends Component {
+
+    // instantiate the props here. 
+    constructor(props) {
+        super(props);
+    }
+
+    // documentaiton says to put this in here.
+    getScanSettings = () => {
+        return new ScanSettings({ enabledSymbologies: [Barcode.Symbology.CODE128] });
+    };
+
+    componentDidMount() {
+        // put stuff in here when the component mounts
+    }
+
     render() {
-        return <HelloWorldSample sampleText={this.props.sampleText} />;
+        return (
+            <ScanditBarcodeScanner
+                // Library licensing & configuration options (see https://docs.scandit.com/stable/web/globals.html#configure)
+                licenseKey={this.licenseKey}
+                engineLocation="https://cdn.jsdelivr.net/npm/scandit-sdk@5.x/build" // could also be a local folder, e.g. "build"
+                // Picker events
+                onReady={() => this.setState({ scannerReady: true })}
+                onScan={console.log}
+                onScanError={console.log}
+                // Picker options
+                scanSettings={this.getScanSettings()}
+            />
+        );
     }
 }
